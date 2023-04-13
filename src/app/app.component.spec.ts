ef178a66e0442bf3ffb7a5ed714ca5b638eb52c1
Chayan Kumar <chayankumar08@gmail.com>
2023-04-14 01:11:57 +0530

    Updates

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'firstApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('firstApp');
+    expect(app.name).toEqual('firstApp');
   });
 
   it('should render title', () => {
